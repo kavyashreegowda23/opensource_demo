4a6882cb91757fc3ac8cc4fd1b3dde946491ed28
bhavya <bhavyasubramanya63@gmail.com>
2024-07-24 12:32:38 +0530

    add 10th and 11 th line

--- lab.py
+++ lab.py
@@ -1,11 +1,6 @@
 print("Good Morning")
-print("OPENSOUREC DEVELPOMENT")
-print("open develpoment")
 print("hello how are uhh")
 print("HELLO WORLD")
-print("hello world")
-print("full stack deveploment")
-print("Computer science and Engg")
-print("vivekananda polytechnic")
-
+print("Kavya,bhavya")
+print("Nandini,Moksha")
 
